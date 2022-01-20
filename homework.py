239c1df9cdaa7a404a3b77f251cb6966e010659c
xrito <88393376+xrito@users.noreply.github.com>
2022-01-20 21:27:32 +0400

    600 sec

--- homework.py
+++ homework.py
@@ -13,7 +13,7 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
-RETRY_TIME = 15
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
